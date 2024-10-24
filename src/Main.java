689b0f4359a1ec2a73ec900179a0e64c826af953
Mariorocke <mariorocke@gmail.com>
2024-10-23 20:06:30 -0600

    Cambios en Main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        String bootcamp = "Java Developer 19";
+        System.out.printf("Hello %s\n",bootcamp);
+
     }
 }
